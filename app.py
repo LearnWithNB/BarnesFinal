f08cc75771ce3bba10d0950b5230f9fe2b534259
LearnWithNB <49893717+LearnWithNB@users.noreply.github.com>
2024-04-11 21:50:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-  return 'Hello Barnes ECS Container!'
+    return 'Hello Barnes ECS Container!'
 
-app.run(host='0.0.0.0')
+# This block ensures that the Flask development server
+# is only started when the script is run directly.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
